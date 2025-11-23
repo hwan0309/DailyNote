1acd127f78b925ec91d6fda96ab9b8f20e7886a8
yoon <protmxk@naver.com>
2025-11-23 21:46:37 +0900

    셀렉트박스 필터링 적용

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,18 +1,44 @@
 import Button from "./Button";
 import "./DiaryList.css";
 import DiaryItem from "./DiaryItem";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  const sortedData = getSortedData();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된 순</option>
         </select>
-        <Button text={"새 다이어리"} type={"POSITIVE"} />
+        <Button
+          onClick={() => nav("/new")}
+          text={"새 다이어리"}
+          type={"POSITIVE"}
+        />
       </div>
       <div className="list_wrapper">
-        {data.map((item) => (
+        {sortedData.map((item) => (
           <DiaryItem key={item.id} {...item} />
         ))}
       </div>
