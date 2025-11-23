c5756e51258a6015c98d44d3d4c86f7f4ab87d0a
yoon <protmxk@naver.com>
2025-11-23 21:06:23 +0900

    리스트 컴포넌트 추가

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,5 +1,21 @@
+import { getEmotionImage } from "../util/get-emotion-image";
+import Button from "./Button";
+import "./DiaryItem.css";
 const DiaryItem = () => {
-  return <div>item</div>;
+  return (
+    <div className="DiaryItem">
+      <div className="img_section">
+        <img src={getEmotionImage(1)} />
+      </div>
+      <div className="info_section">
+        <div className="created_date">{new Date().toLocaleDateString()}</div>
+        <div className="content">다이어리 컨텐츠</div>
+      </div>
+      <div className="button-section">
+        <Button text={"수정하기"} />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryItem;
