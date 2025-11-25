fee0c71f20d10ab18ca0955843f11dc5b2a0eff1
yoon <protmxk@naver.com>
2025-11-25 20:28:30 +0900

    Edit 페이지 추가

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,34 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const Edit = () => {
   const params = useParams();
-  return <div>{params.id}번 일기입니다.</div>;
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={"다이어리수정"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
